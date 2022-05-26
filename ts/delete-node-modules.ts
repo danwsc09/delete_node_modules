1416b4c37d061c80be8298e55f630f2a29de0c1a
Wonsang <sang.jeong@freedgrouptech.com>
2022-05-27 00:00:13 +0900

    feat: implement recursively deleting node_modules

--- ts/delete-node-modules.ts
+++ ts/delete-node-modules.ts
@@ -1,5 +1,21 @@
 import * as fs from 'fs/promises'
 
-const path = ''
+const path = './zzdir'
 
-fs.readdir
+async function deleteNodeModules(path: string): Promise<void> {
+  const entries = await fs.readdir(path, { withFileTypes: true })
+  console.log('entries:', entries)
+  for (const entry of entries) {
+    console.log(`scanning ${entry.name}, ${entry.isDirectory() ? 'dir' : 'file'}`)
+    if (entry.isDirectory()) {
+      if (entry.name === 'node_modules') {
+        console.log('deleting...')
+        await fs.rm(path + '/' + entry.name, { force: true, recursive: true })
+      } else {
+        deleteNodeModules(path + '/' + entry.name)
+      }
+    }
+  }
+}
+
+deleteNodeModules(path)
